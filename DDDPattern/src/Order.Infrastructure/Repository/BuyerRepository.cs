bf214a061a50377c9e45a5e4efa67a927dadcf9c
Ali Ergüç <ergucali1@gmail.com>
2023-08-06 03:28:38 +0300

    Create Buyer and Order Repositories.

---
+++ DDDPattern/src/Order.Infrastructure/Repository/BuyerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Order.Application.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Infrastructure.Repository
+{
+    internal class BuyerRepository : IBuyerRepository
+    {
+        public async Task<int> SaveChangesAsync()
+        {
+            return await Task.FromResult(1);
+        }
+    }
+}
