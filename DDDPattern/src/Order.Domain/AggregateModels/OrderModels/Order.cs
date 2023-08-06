8f4767dc81e5405522e136fb99c8dffcca1d5a48
Ali Ergüç <ergucali1@gmail.com>
2023-08-06 03:06:34 +0300

    Create Adress And Order AggregateModels.

---
+++ DDDPattern/src/Order.Domain/AggregateModels/OrderModels/Order.cs
@@ -0,0 +1,18 @@
+﻿using Order.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.AggregateModels.OrderModels
+{
+    public class Order : IAggregateRoot
+    {
+        public DateTime OrderDate { get; set; }
+        public string Description { get; set; }
+        public string OrderStatus { get; set; }
+        public Guid BuyerId { get; set; }
+        public Adress Adress { get; set; }
+    }
+}
